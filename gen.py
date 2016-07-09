f9f6634f029b0b26cd9d6ba39bb726c262be60b2
h3nnn4n <uber.renan@gmail.com>
2016-07-09 23:23:33 +0000

    Now checks if the input is correct

--- gen.py
+++ gen.py
@@ -2,6 +2,10 @@
 import random
 import sys
 
+if len(sys.argv) is not 4:
+    print("Usage: %s number width heigth" % (sys.argv[0]))
+    exit()
+
 n = int(sys.argv[1])
 x = int(sys.argv[2])
 y = int(sys.argv[3])
